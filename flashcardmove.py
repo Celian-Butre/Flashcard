46903a82ba0f57a3cb8f33e5f15bc2f71cedade5
Celian Butre <58144525+Celian-Butre@users.noreply.github.com>
2023-09-14 09:03:26 +0200

    Add files via upload

---
+++ flashcardmove.py
@@ -0,0 +1,43 @@
+dir = "Poly 4"
+
+import os, random, time
+import warnings
+warnings.filterwarnings('ignore', '.*font.*', )
+
+from PIL import Image
+import cv2
+
+
+ideal_width, ideal_height = 2000, 1400
+
+def openThisScreenshot(file) :
+
+	im4size = cv2.imread(str(dir + "/"+ str(file)))
+
+	true_height, true_width, junk = im4size.shape
+	width_scale = ideal_width / true_width
+
+	height_scale = ideal_height / true_height
+
+	scale = min(width_scale, height_scale)
+
+	new_width, new_height = int(true_width * scale), int(true_height * scale)
+
+	im = Image.open(str(dir + "/"+ str(file))).resize((new_width, new_height))
+
+	im.show()
+
+
+
+folder = random.choice(os.listdir(dir))
+print(str(folder))
+openThisScreenshot(str(folder + "/" + folder + "Q.png"))
+time.sleep(5)
+openThisScreenshot(str(folder + "/" + folder + "A.png"))
+
+a= input("correct = y, false = n, redoLater = l")
+if a == "y":
+	os.system(str("mv "+ '\'' + dir + '\'' + "/"+folder+ " Memorised\ FlashCards/"+'\'' + dir + '\'' ))
+if a == "n":
+	os.system(str("mv " +'\'' + dir + '\'' + "/"+folder+ " NotMem/"+'\'' + dir + '\'' ))
+
